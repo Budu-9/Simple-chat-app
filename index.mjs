4079ecff4f7437edaf44091b91beea22907ef9bc
Abayomi <yomiakinde.ya@gmail.com>
2023-11-21 18:59:20 +0100

    changed index extension to mjs

--- index.mjs
+++ index.mjs
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename)
 const PORT = process.env.PORT || 4000
 const app = express()
 app.use(express.static(path.join(__dirname, "public")))
+
 const expressServer = app.listen(PORT,() => {
     console.log(`listening on port ${PORT}`)
 })
